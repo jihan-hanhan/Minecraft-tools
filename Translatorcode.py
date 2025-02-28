b6891bc283bf82c5b6e8227a5eed78581dfb5253
jihan-hanhan <jihan2111@outlook.com>
2025-02-28 23:38:18 +0800

    Add files via upload

---
+++ Translatorcode.py
@@ -0,0 +1,91 @@
+import os
+import shutil
+import zipfile as zf
+from tkinter import filedialog
+from datetime import datetime
+from tkinter import END
+from tkinter import messagebox
+#import translators as ts
+
+class Trans():
+    def __init__(self,mes):
+        self.filepath=[]
+        self.filelist=[]
+        self.mes = mes
+        self.rmcache()
+        pass
+
+    def cutin(self,str,kind=None,):
+        self.mes.text.configure(state='normal')
+        timenow = datetime.now()
+        timenow = timenow.strftime("%H:%M:%S")
+        self.mes.insert(END,f"[{timenow}]:")
+        if kind == None:
+            self.mes.insert(END,str)
+        elif kind == 1:
+            self.mes.insert(END,str,"warn")
+        self.mes.text.configure(state='disabled')
+
+    def rmcache(self):
+        if os.path.exists("./cache"):
+            shutil.rmtree("./cache")
+        os.mkdir("./cache")
+        self.cutin("旧的缓存已清空\n")
+    
+    def getpath(self):
+        self.filepath.append(filedialog.askopenfilename(title="选择文件",filetypes=[("jar文件","*.jar"),("json文件","*.json")]))
+        if not self.filepath[-1]=="":
+            self.cutin(f"新的文件{os.path.basename(self.filepath[-1])}加入队列\n")
+            file_n , file_e = os.path.splitext(self.filepath[-1])
+            if file_e == ".jar":
+                back = "_jar"
+            elif file_e == ".json":
+                back = "_jn"
+            path_s = (f"./cache/"+os.path.basename(self.filepath[-1])+back)
+            path_s = os.path.join(path_s)
+            
+            if back == "_jar":
+                with zf.ZipFile(self.filepath[-1],"r") as z:
+                    z.extractall(path_s)
+            elif back == "_jn":
+                os.mkdir(path_s)
+                shutil.copy(self.filepath[-1],path_s)
+            self.cutin(f"文件{os.path.basename(self.filepath[-1])}加入缓存\n")
+        else:
+            self.cutin("没有选择文件\n")
+            del self.filepath[-1]
+    
+    def delfile(self):
+        self.filepath = os.listdir("./cache")
+        if self.filepath == []:
+            self.cutin("没有缓存文件\n")
+            return
+        file = filedialog.askdirectory(initialdir="./cache",title="选择缓存文件夹")
+        return_d = messagebox.askquestion("确认","是否删除该缓存")
+        if return_d == "no":
+            self.cutin("用户取消删除\n")
+            return
+        shutil.rmtree(file)
+        self.cutin("删除成功\n")
+    
+    def spdic(self,dic,count):
+        sub_dicts = []
+        keys = list(dic.keys())
+        to_keys = len(keys)
+
+        for i in range(0, to_keys, count):
+            sub_dict = {k: dic[k] for k in keys[i:i + count]}
+            print(i)
+            sub_dicts.append(sub_dict)
+
+        return sub_dicts
+    
+    def trans_ts(self,dec):
+        pass
+
+    
+    
+
+
+        
+        
---
+++ main.py
@@ -0,0 +1,164 @@
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+import darkdetect
+from tkinter.font import Font
+from ttkbootstrap.scrolled import ScrolledText
+from tkinter import filedialog
+from tkinter import Listbox
+from tkinter import Entry
+
+
+from Translatorcode import Trans
+
+
+if darkdetect.isDark() == True:
+    kind = "darkly"
+else:
+    kind = "litera"
+
+def refont(side):
+    returnfont = Font(family="Microsoft YaHei",size=side)
+    return returnfont
+
+root = ttk.Window(themename=kind,title="Minecraft tools")
+root.geometry("1000x650")
+
+nt = ttk.Notebook(root)
+nt.pack(fill="both",expand=True)
+
+#翻译器页面小组件
+p1 = ttk.Frame()
+nt.add(p1,text="翻译器")
+
+title = ttk.Label(p1,text="Minecraft 模组翻译器",font=refont(30))
+title.pack(side=TOP,anchor="w",padx=10)
+
+text_ref = ttk.Label(p1,text="*翻译输出不代表mod创作团队品质",foreground="gray")
+text_ref.pack(side=TOP,anchor="e")
+
+flood = ttk.Progressbar(p1,length=1115,bootstyle="success")
+flood.pack(side=TOP,anchor="w",padx=13,pady=10,fill="x")
+
+logs_text = ScrolledText(p1,vbar=True,width=60, height=28,font=refont(10))
+logs_text.pack(side=LEFT,anchor="nw",padx=11,pady=10)
+logs_text.text.configure(state='disabled')
+logs_text.tag_configure("warn", foreground="#ffc107")
+
+tools = ttk.Frame(p1,width=380,height=680)
+tools.pack(side=LEFT,anchor="n",padx=10,pady=10,fill="y")
+tools.pack_propagate(False)
+
+
+
+##start框架
+text = ttk.Label(tools,text="————开始————",foreground="gray")
+text.pack(side=TOP,anchor="w")
+
+start_frame = ttk.Frame(tools,width=100,height=130)
+start_frame.pack(side=TOP,pady=10,anchor="w")
+
+startbuttun1 = ttk.Button(start_frame,bootstyle="info-outline",width=7,text="翻译单个")
+startbuttun1.pack(side=LEFT,padx=2)
+
+startbuttun2 = ttk.Button(start_frame,bootstyle="info-outline",width=7,text="翻译所有")
+startbuttun2.pack(side=LEFT,padx=2)
+
+startbuttun3 = ttk.Button(start_frame,bootstyle="info-outline",width=7,text="翻译json")
+startbuttun3.pack(side=LEFT,padx=2)
+
+
+##output框架
+text = ttk.Label(tools,text="————输出————",foreground="gray")
+text.pack(side=TOP,anchor="w")
+
+output_frame = ttk.Frame(tools,width=100,height=130)
+output_frame.pack(side=TOP,pady=10,anchor='w')
+
+
+opbuttun1 = ttk.Button(output_frame,bootstyle="success-outline",width=7,text="jar")
+opbuttun1.pack(side=LEFT,padx=2)
+
+opbuttun2 = ttk.Button(output_frame,bootstyle="success-outline",width=7,text="json")
+opbuttun2.pack(side=LEFT,padx=2)
+
+opbuttun3 = ttk.Button(output_frame,bootstyle="success-outline",width=7,text="材质包")
+opbuttun3.pack(side=LEFT,padx=2)
+
+#文件操作
+text = ttk.Label(tools,text="————文件————",foreground="gray")
+text.pack(side=TOP,anchor="w")
+
+fileframe = ttk.Frame(tools,)
+fileframe.pack(side=TOP,pady=10,anchor='w')
+
+input_buttun = ttk.Button(fileframe,bootstyle="info-outline",width=7,text="选择文件",command=lambda:mian_code.getpath())
+input_buttun.pack(side=LEFT,padx=2)
+
+del_buttun = ttk.Button(fileframe,bootstyle="danger-outline",width=7,text="删除文件",command=lambda:mian_code.delfile())
+del_buttun.pack(side=LEFT,padx=2)
+
+##settings&list
+text = ttk.Label(tools,text="————设置————",foreground="gray")
+text.pack(side=TOP,anchor="w")
+
+fromlanguage = "en"
+tolanguage = "zh-cn"
+
+def setting():
+    window_set = ttk.Toplevel(root)
+    window_set.title("设置")
+    window_set.geometry("300x500")
+
+    text = ttk.Label(window_set,text="使用翻译库:")
+    text.pack(side="top",anchor="w",padx=5)
+    combobox_ta = ttk.Combobox(window_set,values=["translators(联网)","argostranslate(本地模型)"],state="readonly",bootstyle="danger")
+    combobox_ta.current(0)
+    combobox_ta.pack(side=TOP,anchor="w",padx=5)
+
+    #translators
+    text = ttk.Label(window_set,text="translators库相关:")
+    text.pack(side="top",anchor="w",padx=5)
+    trframe = ttk.Frame(window_set)
+    trframe.pack(side="top",anchor="w",padx=10)
+
+    text = ttk.Label(trframe,text="翻译引擎")
+    text.pack(side="top",anchor="w",padx=5)
+    combobox = ttk.Combobox(trframe,values=["Niutrans", "MyMemory", "Alibaba", "Baidu", "ModernMt", "VolcEngine"
+                                            , "Iciba", "Iflytek", "Google", "Bing", "Lingvanex", "Yandex", "Itranslate",
+                                              "SysTran", "Argos", "Apertium", "Reverso", "Deepl", "CloudTranslation",
+                                            "QQTranSmart", "TranslateCom", "Sogou", "Tilde", "Caiyun", "QQFanyi",
+                                              "TranslateMe", "Papago", "Mirai", "Youdao", "Iflyrec", "Hujiang", "Yeekit",
+                                                "LanguageWire", "Elia", "Judic", "Mglip", "Utibet"],state="readonly")
+    combobox.current(9)
+    combobox.pack(side="top",anchor="w",padx=5)
+    text = ttk.Label(trframe,text="*更多请访问 https://github.\ncom/UlionTse/translators/ \n了解",foreground="gray")
+    text.pack(side="top",anchor="w",padx=5)
+    
+    text = ttk.Label(trframe,text="输入语言")
+    text.pack(side="top",anchor="w",padx=5)
+    e1 = ttk.StringVar()
+    entry1 = Entry(trframe,width=10,textvariable=e1)
+    entry1.pack(side="top",anchor="w",padx=5)
+    e1.set(fromlanguage)
+
+    text = ttk.Label(trframe,text="输出语言")
+    text.pack(side="top",anchor="w",padx=5)
+    e2 = ttk.StringVar()
+    entry2 = Entry(trframe,width=10,textvariable=e2)
+    entry2.pack(side="top",anchor="w",padx=5)
+    e2.set(tolanguage)
+
+    text = ttk.Label(trframe,text="*注意,请使用\"-\"而不是\"_\"",foreground="gray")
+    text.pack(side="top",anchor="w",padx=5)
+
+
+setbuttun = ttk.Button(tools,bootstyle="info-outline",width=7,text="设置",command=setting)
+setbuttun.pack(side=TOP,pady=10,anchor='w')
+
+
+
+
+
+#函数/主程序
+mian_code = Trans(mes=logs_text)
+root.mainloop()
